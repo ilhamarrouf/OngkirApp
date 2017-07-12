a0cd603815fe168b1f25cce6a1a4e0cb10d349e5
ilhamarrouf <ilham.arrouf@gmail.com>
2017-07-12 15:29:19 +0700

    add route docs

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,11 @@
 */
 
 $app->get('/', function ($request, $response) {
-    return ':)';
+    return 'For documentation : '. $_SERVER['SERVER_NAME'].'/docs :)';
+});
+
+$app->get('/docs', function ($request, $response) {
+    return $response->withRedirect('https://gist.github.com/ilhamarrouf/f4603cb0322d106ed5d9a6ed5025ec35');
 });
 
 $app->group('/api', function () use ($app) {
