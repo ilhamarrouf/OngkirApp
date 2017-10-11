3d521ae93d9b55c2c29b17053ce4c82b20e934fb
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2017-10-11 17:20:06 +0700

    add cors

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -41,3 +41,21 @@ $app->group('', function () use ($app) {
 $app->group('/api', function () use ($app) {
     require __DIR__ . './../src/routesAPI.php';
 })->add(new \App\Middlewares\ApiRoute($container));
+
+/*
+|--------------------------------------------------------------------------
+| Cross origin resource sharing
+|--------------------------------------------------------------------------
+*/
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
