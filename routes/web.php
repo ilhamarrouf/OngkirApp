c37247dce8626ddae31f0d660bbdcf609b3ee196
ihamarrouf <ilham.arrouf@gmail.com>
2018-07-15 19:32:10 +0700

    link for documentation

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@
 */
 
 $app->get('/', function ($request, $response) {
-    return 'For documentation : '. $_SERVER['SERVER_NAME'].'/docs :)';
+    return 'For documentation : <a href="'.$this->router->pathFor('docs').'">link</a> :)';
 });
 
 $app->get('/docs', function ($request, $response) {
     return $response->withRedirect('https://gist.github.com/ilhamarrouf/f4603cb0322d106ed5d9a6ed5025ec35');
-});
+})->setName('docs');
